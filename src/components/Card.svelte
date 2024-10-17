274b8d155c7b501caa4e4c59a4e55ec60f0fdaae
obed-tc <castroobed53@gmail.com>
2024-10-17 13:09:05 -0400

    first commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,130 @@
+
+
+<script>
+// @ts-nocheck
+import Trebol from "../assets/trebol.svg";
+import Corazon from "../assets/Corazon.svg";
+import Picas from "../assets/picas.svg";
+import Diamante from "../assets/diamante.svg";
+    export let numberCard;
+    export let sign;
+    // let numberCard=3;
+
+    let icono=sign=="picas"?Picas:sign=="trebol"?Trebol:sign=="corazon"?Corazon:Diamante;
+    let cards={
+
+        1:"A",
+        2:"2",
+        3:"3",
+        4:"4",
+        5:"5",
+        6:"6",
+        7:"7",
+        8:"8",
+        9:"9",
+        10:"10",
+        11:"J",
+        12:"Q",
+        13:"K",
+
+};
+</script>
+
+<div class="bg-white shadow-xl rounded-lg min-w-[15vw] max-w-[15vw] relative h-[50vh] p-3 flex flex-col">
+
+    <div class=" ">
+        <p  class="font-bold ml-1">{cards[numberCard]}</p>
+        <img src={icono} class="w-[20px]" alt="">
+
+    </div>
+<div class="flex-1">
+    {#if numberCard<=3}
+        <div class="flex flex-col h-full items-center {numberCard==1?'justify-center':'justify-between'}">
+            {#each Array(numberCard) as _,index}
+
+            
+        <img src={icono} class="{numberCard!=1?'w-[35px]':'w-[100px]'}" alt="">
+            {/each}
+        </div>
+    {/if}
+
+    {#if numberCard>=4 && numberCard<10}
+    <div class="flex  h-full items-center justify-around px-5">
+        <div class="flex flex-col justify-between h-full">
+            {#each Array(parseInt((numberCard/2).toString())) as _,index}
+        <img src={icono} class="w-[35px]" alt="">
+
+        {/each}
+        </div>
+        {#if numberCard%2!=0}
+        <img src={icono} class="w-[35px]" alt="">
+
+        {/if}
+        <div class="flex flex-col justify-between h-full">
+        {#each Array(parseInt((numberCard/2).toString())) as _,index}
+        <img src={icono} class="w-[35px]" alt="">
+
+        {/each}
+        </div>
+    </div>
+    {/if}
+
+    <div class="h-full flex flex-col justify-between  space-y-7">
+
+    {#if numberCard==10}
+        {#each Array(parseInt((numberCard/4).toString())) as _,index}
+            <div class="flex  h-full items-center justify-around px-5">
+                <div class="flex flex-col justify-between h-full">
+                    {#each Array(parseInt((numberCard/4).toString())) as _,index}
+        <img src={icono} class="w-[35px]" alt="">
+
+                {/each}
+                </div>
+                {#if (numberCard/2)%2!=0}
+        <img src={icono} class="w-[35px]" alt="">
+
+                {/if}
+                <div class="flex flex-col justify-between h-full">
+                {#each Array(parseInt((numberCard/4).toString())) as _,index}
+        <img src={icono} class="w-[35px]" alt="">
+
+                {/each}
+                </div>
+            </div>
+        
+        {/each}
+    {/if}
+
+{#if numberCard>=11}
+<div class="flex flex-col items-center justify-center h-full">
+    <p class="text-[50px] font-bold">{cards[numberCard]}</p>
+
+    <img src={icono} class="w-[50px] -mt-1" alt="">
+<!-- 
+    {#if cards[numberCard]=="J"}
+    <img src="" alt="J">
+        
+    {/if}
+    {#if cards[numberCard]=="Q"}
+    <img src="" alt="J">
+        
+    {/if}
+    {#if cards[numberCard]=="K"}
+    <img src="" alt="J">
+        
+    {/if} -->
+
+</div>
+    
+{/if}
+</div>
+
+
+</div>
+
+    <div class="  rotate-180 ">
+        <p  class="font-bold ml-1">{cards[numberCard]}</p>
+        <img src={icono} class="w-[20px]" alt="">
+
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,103 @@
+
+<script lang="ts">
+  import Card from "../components/Card.svelte";
+
+
+    let cardSelected:any=null;
+    let cartas = [
+    { numberCard: 1, sign: "trebol" },
+    { numberCard: 2, sign: "trebol" },
+    { numberCard: 3, sign: "trebol" },
+    { numberCard: 4, sign: "trebol" },
+    { numberCard: 5, sign: "trebol" },
+    { numberCard: 6, sign: "trebol" },
+    { numberCard: 7, sign: "trebol" },
+    { numberCard: 8, sign: "trebol" },
+    { numberCard: 9, sign: "trebol" },
+    { numberCard: 10, sign: "trebol" },
+    { numberCard: 11, sign: "trebol" }, // Sota
+    { numberCard: 12, sign: "trebol" }, // Caballo
+    { numberCard: 13, sign: "trebol" }, // Rey
+    { numberCard: 1, sign: "diamante" },
+    { numberCard: 2, sign: "diamante" },
+    { numberCard: 3, sign: "diamante" },
+    { numberCard: 4, sign: "diamante" },
+    { numberCard: 5, sign: "diamante" },
+    { numberCard: 6, sign: "diamante" },
+    { numberCard: 7, sign: "diamante" },
+    { numberCard: 8, sign: "diamante" },
+    { numberCard: 9, sign: "diamante" },
+    { numberCard: 10, sign: "diamante" },
+    { numberCard: 11, sign: "diamante" }, // Sota
+    { numberCard: 12, sign: "diamante" }, // Caballo
+    { numberCard: 13, sign: "diamante" }, // Rey
+    { numberCard: 1, sign: "corazon" },
+    { numberCard: 2, sign: "corazon" },
+    { numberCard: 3, sign: "corazon" },
+    { numberCard: 4, sign: "corazon" },
+    { numberCard: 5, sign: "corazon" },
+    { numberCard: 6, sign: "corazon" },
+    { numberCard: 7, sign: "corazon" },
+    { numberCard: 8, sign: "corazon" },
+    { numberCard: 9, sign: "corazon" },
+    { numberCard: 10, sign: "corazon" },
+    { numberCard: 11, sign: "corazon" }, // Sota
+    { numberCard: 12, sign: "corazon" }, // Caballo
+    { numberCard: 13, sign: "corazon" }, // Rey
+    { numberCard: 1, sign: "diamante" },
+    { numberCard: 2, sign: "diamante" },
+    { numberCard: 3, sign: "diamante" },
+    { numberCard: 4, sign: "diamante" },
+    { numberCard: 5, sign: "diamante" },
+    { numberCard: 6, sign: "diamante" },
+    { numberCard: 7, sign: "diamante" },
+    { numberCard: 8, sign: "diamante" },
+    { numberCard: 9, sign: "diamante" },
+    { numberCard: 10, sign: "diamante" },
+    { numberCard: 11, sign: "diamante" }, // Sota
+    { numberCard: 12, sign: "diamante" }, // Caballo
+    { numberCard: 13, sign: "diamante" }  // Rey
+];
+
+    let cartasGame = [...cartas];
+
+
+
+    function getCardPlayer(){
+        let randomIndex = Math.floor(Math.random() * cartasGame.length);
+        cardSelected = cartasGame[randomIndex];
+        cartasGame.splice(randomIndex, 1);
+        cartasGame = [...cartasGame];
+
+    
+    console.log(`Carta seleccionada:`, cardSelected);
+    console.log(`Cartas restantes:`, cartasGame);
+
+    }
+
+
+</script>
+
+<div class=" bg-gray-200 min-h-screen">
+{cartas.length}
+    <!-- <div class="flex">
+        {#each cartas as carta }
+        <Card  numberCard={carta.numberCard} sign={carta.sign}></Card>
+        {/each}
+    </div> -->
+
+{#if cardSelected}
+    <Card  numberCard={cardSelected.numberCard} sign={cardSelected.sign}></Card>
+    
+{/if}
+
+<br/>
+
+
+    <button class="bg-green-500 py-2 px-6 rounded-full text-white cursor-pointer absolute z-20" on:click={getCardPlayer}>
+        Iniciar
+        {cartasGame.length}
+
+    </button>
+
+</div>
